8219ebaab8d810e2d937922061e8ab3a990fd4a8
jeroen <jpjagt@pm.me>
2024-03-16 16:43:08 +0100

    add sonner, and correct globals.css import

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import GlobalStyles from '@/components/GlobalStyles'
 import { ThemeProvider } from '@/components/ThemeProvider'
+import { Toaster } from '@/components/ui/sonner'
+import './globals.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Toaster />
       <GlobalStyles />
       <App />
     </ThemeProvider>
